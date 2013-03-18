6b4e447429c8cfc8d77d2bfb8c72507773914ca4
Brian Lam <blam0707@gmail.com>
2013-03-18 02:03:25 -0400

    Beginning stages of the color matching
    Not totally sure how its going to be implemented yet

---
+++ ColorCode/InitializeKeyTerms.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace ColorCode
+{
+
+    class InitializeKeyTerms
+    {
+        HashSet<string> javaSet_type;
+        HashSet<string> javaSet_cond;
+
+        public InitializeKeyTerms()
+        {
+            javaSet_type = new HashSet<string>();
+            javaSet_cond = new HashSet<string>();
+
+        }
+    }
+}
