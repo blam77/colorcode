3d229d82a1745aabe3799bf9c67fc6682f9309c7
Brian Lam <blam0707@gmail.com>
2013-03-25 03:29:11 -0400

    Got color matching to work for all words!
    A teensy bit slow but I think it is good, it works from my testing for
    the words in the hashsets

--- ColorCode/CodeEditor.xaml.cs
+++ ColorCode/CodeEditor.xaml.cs
@@ -31,6 +31,8 @@ namespace ColorCode
         //string textFromCodePad;
         StorageFile globalFile;
         Boolean isCtrlKeyPressed;
+        List<int> highlights;
+        List<string> wordsToHighlight;
         //string textFromRichPad;
 
         public CodeEditor()
@@ -65,12 +67,12 @@ namespace ColorCode
             if (e.Key == Windows.System.VirtualKey.Control) isCtrlKeyPressed = false;
 
 
-            string str;
+            /*string str;
             RichCodePad.Document.GetText(Windows.UI.Text.TextGetOptions.None, out str);
             List<int> intz = new List<int>();
             List<string> words = new List<string>();
             List<int> intz_visited = new List<int>();
-            foreach (Match match in Regex.Matches(str, @"\sint\b|\bint\s|\bint\\;?"))
+            foreach (Match match in Regex.Matches(str, @"\s" + "int" + @"\b|\bint\s|\bint\\;?"))
             {
                     intz.Add(match.Index);
                     words.Add(match.ToString());
@@ -96,42 +98,10 @@ namespace ColorCode
                 }
 
                 select.StartPosition = selectTwo;
-            }
+            }*/
+            if (e.Key == Windows.System.VirtualKey.Space || e.Key == VirtualKey.Enter)
+                syntax_highlight();
        }
-
-        
-
-        /*private void CodePad_KeyDown(object sender, KeyRoutedEventArgs e)
-        {
-            if (e.Key == Windows.System.VirtualKey.Tab)
-            {
-                //Testing tab funcationality
-                int tempPos = CodePad.SelectionStart;
-
-                //First half of the document
-                CodePad.Select(0, tempPos);
-                String firstString = CodePad.SelectedText;
-
-                //Second half of the document
-                CodePad.Select(tempPos, CodePad.Text.Length);
-                String secondString = CodePad.SelectedText;
-
-                //Add firstString together with a 'tab'
-                CodePad.Text = firstString + "\t";
-
-                //save cursor to the after tab spot
-                int tempPos2 = firstString.Length + 1;
-
-                //add the second half of the text back
-                CodePad.Text += secondString;
-
-                //set cursor back to tab spot
-                CodePad.SelectionStart = tempPos2;
-
-                textFromCodePad = CodePad.Text;
-                //return CodePad.Text;
-            }
-        }*/
 
         private void RichPad_KeyDown(object sender, KeyRoutedEventArgs e)
         {
@@ -149,7 +119,7 @@ namespace ColorCode
                 //First half of the document
                 var firstRange = RichCodePad.Document.GetRange(0, tempPos);
                 String firstString;
-                firstRange.GetText(Windows.UI.Text.TextGetOptions.None, out firstString); ;
+                firstRange.GetText(Windows.UI.Text.TextGetOptions.None, out firstString);
 
                 //Second half of the document
                 var secondRange = RichCodePad.Document.GetRange(tempPos, body.Length);
@@ -165,6 +135,9 @@ namespace ColorCode
                 //save cursor to the after tab spot
                 RichCodePad.Document.SetText(Windows.UI.Text.TextSetOptions.None, body);
                 RichCodePad.Document.Selection.StartPosition = firstString.Length + 1;
+
+                //trying to fix highlighting with tab
+                syntax_highlight();
             }
             if (e.Key == Windows.System.VirtualKey.Control) isCtrlKeyPressed = true;
             else if (isCtrlKeyPressed)
@@ -182,6 +155,56 @@ namespace ColorCode
 
             }
         }
+
+        private void syntax_highlight()
+        {
+            string str;
+            highlights = new List<int>();
+            List<int> highlights2 = new List<int>();
+            wordsToHighlight = new List<string>();
+            List<string> wordsToHighlight2 = new List<string>();
+            RichCodePad.Document.GetText(Windows.UI.Text.TextGetOptions.None, out str);
+            var selectTwo = RichCodePad.Document.Selection.StartPosition;
+            var select1 = RichCodePad.Document.Selection;
+
+                foreach (string s in App.terms.javaSet_type)
+                {
+                    foreach (Match match in Regex.Matches(str, @"\s" + s + @"\b|\b" + s + @"\s|\b" + s + @"\\;?"))
+                    {
+                        highlights.Add(match.Index);
+                        wordsToHighlight.Add(match.ToString());
+                    }
+                }
+
+                foreach (string s in App.terms.javaSet_cond)
+                {
+                    foreach (Match match in Regex.Matches(str, @"\s" + s + @"\b|\b" + s + @"\s|\b" + s + @"\\;?|\b"+s+@"\\{?"))
+                    {
+                        highlights2.Add(match.Index);
+                        wordsToHighlight2.Add(match.ToString());
+                    }
+                }
+
+                //color the textbox black before you recolor blue
+                RichCodePad.Document.GetRange(0, str.Length).CharacterFormat.ForegroundColor = Windows.UI.Colors.Black;
+
+                for (int i = 0; i < highlights.Count; i++)
+                {
+                    select1.StartPosition = highlights[i];
+                    select1.EndPosition = highlights[i] + wordsToHighlight[i].Length;
+                    RichCodePad.Document.GetRange(select1.StartPosition, select1.EndPosition).CharacterFormat.ForegroundColor = Windows.UI.Colors.Blue;
+                }
+                for (int h = 0; h < highlights2.Count; h++)
+                {
+                    select1.StartPosition = highlights2[h];
+                    select1.EndPosition = highlights2[h] + wordsToHighlight2[h].Length;
+                    RichCodePad.Document.GetRange(select1.StartPosition, select1.EndPosition).CharacterFormat.ForegroundColor = Windows.UI.Colors.Red;
+                }
+            
+
+            select1.StartPosition = selectTwo;
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
         }
